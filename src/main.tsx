729513418a736bc7f7a44f7e4594e00eee240e1f
the-greenwell <ant.greenwell2@gmail.com>
2023-01-05 20:48:23 -0800

    gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { AppProvider } from './context/AppContext'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AppProvider>
         <App />
       </AppProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
